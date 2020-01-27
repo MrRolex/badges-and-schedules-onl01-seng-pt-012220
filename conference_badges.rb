c5f9ec232f4bd146be0e0cc2337160545aa2a33b
Edward Williams <eddie_w38@yahoo.com>
2020-01-27 16:26:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each (|badge| puts badge)
+  batch_badge_creator(attendees).each(|badge| puts badge)
   assign_rooms(attendees).each(|rooms| puts rooms)
   end
  
